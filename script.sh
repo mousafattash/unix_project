915790a20f6dfd51800f80dbaf4b9760d74430d7
Your Name <mousa@gmail.com>
2025-01-17 11:30:54 +0200

    Automated commit by script

--- script.sh
+++ script.sh
@@ -1,18 +1,21 @@
 COMMIT_MESSAGE="Automated commit by script"
 JENKINS_JOB_NAME="unix"
 JENKINS_URL="http://192.168.1.14:8080"
-JENKINS_CLI_JAR="jenkins-cli.jar"
 JENKINS_USER="root"
 JENKINS_TOKEN="1104b117c9cc42339592cb2a7ce735056f"
 
-git add . || { echo "Failed to add files to Git"; exit 1; }
 
+git add . || { echo "Failed to add files to Git";  }
+git commit -m "$COMMIT_MESSAGE" || { echo "Failed to commit changes"; }
+git push || { echo "Failed to push changes"; }
 
-git commit -m "$COMMIT_MESSAGE" || { echo "Failed to commit changes"; exit 1; }
-
-git push  ||  echo "Failed to push changes"
 
 echo "Triggering Jenkins job: $JENKINS_JOB_NAME"
-java -jar "$JENKINS_CLI_JAR" -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_TOKEN" build "$JENKINS_JOB_NAME" || { echo "Failed to trigger Jenkins job";}
+JENKINS_JOB_URL="$JENKINS_URL/job/$JENKINS_JOB_NAME/build?token=$JENKINS_TOKEN"
+
+# Send POST request to trigger the Jenkins job
+curl -X POST "$JENKINS_JOB_URL" \
+    --user "$JENKINS_USER:$JENKINS_TOKEN" \
+    --fail || { echo "Failed to trigger Jenkins job"; }
 
 echo "Jenkins job triggered successfully."
